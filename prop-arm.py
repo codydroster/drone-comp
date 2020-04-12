57bf2072d5578fc8772de2cb5e78e4e902c1008b
codydroster <cdroster@gmail.com>
2020-04-12 12:47:53 -0500

    prop-arm

---
+++ prop-arm.py
@@ -0,0 +1,42 @@
+from inputs import get_gamepad
+import serial
+import serial.tools.list_ports
+import threading
+from time import sleep
+
+
+class droneArm:
+
+	pitch = 0
+	
+	
+
+
+	def __init__(self):
+		
+		self.ser = serial.Serial('/dev/ttyS0')
+		self.ser.baudrate = 115200
+
+
+drone1 = droneArm()
+
+
+
+
+
+while 1:
+	
+
+	drone1.ser.write(b'hi')
+	
+
+
+	
+
+	
+
+	
+		
+
+
+
