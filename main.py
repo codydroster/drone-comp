3c0f6a1c14b3638fc28f907f59d54a82d66ecddf
Cody Droster <cdroster@gmail.com>
2020-08-28 18:04:23 -0500

    receive gps working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from inputs import get_gamepad
+from inputs import devices 
 import serial
 import serial.tools.list_ports
 import threading
@@ -82,83 +82,77 @@ class Transmitter:
 	def update_gamepad(self):
 		while 1:
 	
-			events = get_gamepad()
-			for event in events:
-				if event.code is "ABS_X":
-					self.raw_yaw = event.state
-					
-				if event.code is "ABS_Y":
-					self.raw_throttle = event.state
 
-				if event.code is "ABS_RX":
-					self.raw_pitch = event.state
-
-				if event.code is "ABS_Z":
-					self.raw_roll = event.state
+			events = devices.gamepads[0]._do_iter()
+			if events is not None:
+				for event in events:
+					if event.code is "ABS_X":
+						self.raw_yaw = event.state	
+					if event.code is "ABS_Y":
+						self.raw_throttle = event.state
+					if event.code is "ABS_RX":
+						self.raw_pitch = event.state
+					if event.code is "ABS_Z":
+						self.raw_roll = event.state						
+					if event.code is "ABS_RY":
+						self.raw_sw_c = event.state			
+					if event.code is "ABS_RZ":
+						self.raw_sw_d = event.state
 					
-				if event.code is "ABS_RY":
-					self.raw_sw_c = event.state
-			
-				if event.code is "ABS_RZ":
-					self.raw_sw_d = event.state
+
+				self.throttle = int(2000/1362 * (self.raw_throttle - 342))
+				self.yaw = int(2000/1362 * (self.raw_yaw - 342))
+				self.pitch = int(2000/1362 * (self.raw_pitch - 342))
+				self.roll = int(2000/1362 * (self.raw_roll - 342))
 				
-
-			self.throttle = int(2000/1362 * (self.raw_throttle - 342))
-			self.yaw = int(2000/1362 * (self.raw_yaw - 342))
-			self.pitch = int(2000/1362 * (self.raw_pitch - 342))
-			self.roll = int(2000/1362 * (self.raw_roll - 342))
-			if(self.raw_sw_c > 45):
-				self.sw_c = 0
-			else:
-				self.sw_c = 1000
-			
-			if(self.raw_sw_d < 45):
-				self.sw_d = 2000
-			elif(210 > self.raw_sw_d > 45):
-				self.sw_d = 1000
-			elif(self.raw_sw_d > 210):
-				self.sw_d = 0
+				if(self.raw_sw_c > 45):
+					self.sw_c = 0
+				else:
+					self.sw_c = 1000
+				
+				if(self.raw_sw_d < 45):
+					self.sw_d = 2000
+				elif(210 > self.raw_sw_d > 45):
+					self.sw_d = 1000
+				elif(self.raw_sw_d > 210):
+					self.sw_d = 0
 
 
 
 def serial_handler():
 
-	while 1:
+#	while 1:
 
-		while(drone1.ser.in_waiting is 0):
-			None
+	#	while(drone1.ser.in_waiting is 0):
+	#		None
 
+	if(drone1.ser.in_waiting > 2):
+	
 		read0 = drone1.ser.read()
-	#	print(read0)
+		read1 = drone1.ser.read()
+
+	
+		if(read0 == b'B' and read1 ==b'C'):
+
+			rx_aligned = drone1.ser.read(8)
+			drone1.roll = int.from_bytes(rx_aligned[0:2], byteorder='big', signed='true')
+			drone1.pitch = int.from_bytes(rx_aligned[2:4], byteorder='big', signed='true')
+			drone1.heading = int.from_bytes(rx_aligned[4:6], byteorder='big', signed='true') 
+			drone1.altitude = int.from_bytes(rx_aligned[6:8], byteorder='big', signed='true')
+
 		
-		if(read0 == b'B'):
+			#great time to transmit, next ~600 byte packet 50ms away
+			txbytes = trans_real.transmit_bytes()
 
-			while(drone1.ser.in_waiting is 0):
-				None
-				
-			read1 = drone1.ser.read()
+			#send to drone
+			drone1.ser.write(txbytes)
 
-			if(read1 == b'C'):
-				rx_aligned = drone1.ser.read(8)
-				drone1.roll = int.from_bytes(rx_aligned[0:2], byteorder='big', signed='true')
-				drone1.pitch = int.from_bytes(rx_aligned[2:4], byteorder='big', signed='true')
-				drone1.heading = int.from_bytes(rx_aligned[4:6], byteorder='big', signed='true') 
-				drone1.altitude = int.from_bytes(rx_aligned[6:8], byteorder='big', signed='true')
-
-				#print(trans_real.transmit_bytes())
-				#great time to transmit, next ~600 byte packet 50ms away
-				txbytes = trans_real.transmit_bytes()
-				#print(txbytes)
-				drone1.ser.write(txbytes)
-				while(drone1.ser.out_waiting is not 0):
-					None
-				
-			else:
-				f.write(read0)
-				f.write(read1)
-		else:
 			
+		else:
 			f.write(read0)
+			f.write(read1)
+	
+
 
 
 os.remove('/home/cody/git/rtk/rover.ubx')
@@ -166,12 +160,16 @@ f = open('/home/cody/git/rtk/rover.ubx', 'wb')
 drone1 = Drone()
 trans_real = Transmitter()
 
+#inputs module blocks
 threading.Thread(target=trans_real.update_gamepad).start()
 
 
 
 while 1:
+
 	serial_handler()
+
+	print(trans_real.sw_c)
 	
 	
 
