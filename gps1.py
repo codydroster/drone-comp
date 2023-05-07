3d71526ef3983488122165ab55801e0234f9db7b
Cody Droster <codydroster@Codys-Air.attlocal.net>
2023-05-07 09:37:40 -0500

    gps.py updates

--- gps1.py
+++ gps1.py
@@ -1,39 +1,17 @@
 import io
 import os
 import time
+from gps import GPS
 
-class GPS:
-	def __init__(self, filepath):
-		self.lat = 0		
-		self.long = 0
-		self.alt = 0
 
-		self.filepath = filepath
-		self.file = open(self.filepath, 'r+')
-		self.file.readlines()
-		self.currentPos = self.file.tell()
-    
-	def getGPS(self):
-		with open(self.filepath, 'r+') as f:
-			f.seek(self.currentPos, 0)
-			line = f.readline()
-			self.currentPos = f.tell()
-			if line != '':
-				fields = line.split()
-				self.lat = float(fields[2])
-				self.long = float(fields[3])
-				self.alt = float(fields[4])
-
-				
-
-gps = GPS('./position_data/rov.pos')
+gps = GPS('./position_data/rover.pos')
+gpsTarget = GPS('./position_data/target.pos')
 
 while True:
-    gps.getGPS()
-    print(gps.lat)
-    time.sleep(.01)
-   
-   
+	gps.setFakeGPS()
+	gpsTarget.setFakeGPS()
+	print(gps.lat)
+	time.sleep(.2)
    
    
    
